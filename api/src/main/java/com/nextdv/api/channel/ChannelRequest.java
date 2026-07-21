898f451ea6c7e21abc522d75f5792e745dca097b
LeeJungBum <jungbum05114@gmail.com>
2026-07-22 00:36:57 +0900

    feat(channel): 채널 생성/조회/삭제 API 레이어 추가, ChannelRequest @Getter 적용

--- api/src/main/java/com/nextdv/api/channel/ChannelRequest.java
+++ api/src/main/java/com/nextdv/api/channel/ChannelRequest.java
@@ -3,27 +3,13 @@ package com.nextdv.api.channel;
 import com.nextdv.domain.channel.ChannelType;
 import java.util.Map;
 import java.util.UUID;
+import lombok.Getter;
 
+@Getter
 public class ChannelRequest {
 
   private UUID userId;
   private ChannelType type;
   private String name;
   private Map<String, Object> config;
-
-  public UUID getUserId() {
-    return userId;
-  }
-
-  public ChannelType getType() {
-    return type;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public Map<String, Object> getConfig() {
-    return config;
-  }
 }
