05f704d4b02575f2f117ec830535730d6f4d0fdb
LeeJungBum <jungbum05114@gmail.com>
2026-08-03 17:03:45 +0900

    chore(seed): PlatformSeeder 제거 및 관련 코드 정리

--- infrastructure/src/main/java/com/nextdv/infrastructure/platform/PlatformJpaRepository.java
+++ infrastructure/src/main/java/com/nextdv/infrastructure/platform/PlatformJpaRepository.java
@@ -4,6 +4,4 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlatformJpaRepository extends JpaRepository<PlatformEntity, UUID> {
-
-  boolean existsByName(String name);
 }
